ff6bbed6e33ca943220beb6d02f16d4be404f721
Alireza <alireza.banaei@atih.sante.fr>
2016-01-07 22:20:04 +0100

    Bricoles

--- pmsi/rehosp/rsa_tools.py
+++ pmsi/rehosp/rsa_tools.py
@@ -9,6 +9,7 @@ from scipy import sparse
 from scipy.sparse import hstack
 import formats
 
+
 def save_sparse_csr(filename, array):
     np.savez(filename, data=array.data, indices=array.indices, indptr=array.indptr, shape=array.shape)
 
