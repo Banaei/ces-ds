65fdb60928241b8f858ee065fc55bf7af7f8214b
Alireza <alireza.banaei@atih.sante.fr>
2016-01-19 22:21:37 +0100

    Generation and saving of first two data sets

--- projet_ds/src/work.py
+++ projet_ds/src/work.py
@@ -19,6 +19,11 @@ selected_ano_hashes_file_path = '/DS/data/pmsi/selected_ano_hash.txt'
 training_sample_proportion = 0.01
 validation_sapmle_proportion = 0.05
 
+X_1_fil_path = '/DS/data/pmsi/x_1'
+y_1_fil_path = '/DS/data/pmsi/y_1'
+X_2_fil_path = '/DS/data/pmsi/x_2'
+y_2_fil_path = '/DS/data/pmsi/y_2'
+
 pmsi_tools.init()
 
 training_anos = pmsi_tools.rand_select_anos(ano_file_path_2013, formats.ano_2013_format, rsa_file_path_2013, formats.rsa_2013_format, training_sample_proportion)
@@ -28,3 +33,11 @@ training_s_X, training_s_y = pmsi_tools.get_sparse_X_y_from_data(training_result
 validation_anos = pmsi_tools.rand_select_anos(ano_file_path_2013, formats.ano_2013_format, rsa_file_path_2013, formats.rsa_2013_format, validation_sapmle_proportion, exclusion_set=training_anos)
 validation_result_dict = pmsi_tools.sample_ano_rsa(ano_file_path_2013, formats.ano_2013_format, rsa_file_path_2013, formats.rsa_2013_format, inclusion_anos_set=validation_anos)
 validation_s_X, vaidation_s_y = pmsi_tools.get_sparse_X_y_from_data(validation_result_dict)
+
+
+pmsi_tools.save_sparse(X_1_fil_path, training_s_X.tocsr())
+pmsi_tools.save_sparse(y_1_fil_path, training_s_y.tocsr())
+pmsi_tools.save_sparse(X_2_fil_path, validation_s_X.tocsr())
+pmsi_tools.save_sparse(y_2_fil_path, vaidation_s_y.tocsr())
+
+
