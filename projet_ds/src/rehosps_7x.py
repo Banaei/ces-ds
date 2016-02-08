2cb36bc08480def12b8bd162bf8a517dfbf99827
Alireza <alireza.banaei@atih.sante.fr>
2016-02-08 10:07:15 +0100

    Debut de devs pour trouver pourquoi il y a des piques de rehosp à des multiples de 7 j

---
+++ projet_ds/src/rehosps_7x.py
@@ -0,0 +1,205 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 08 08:51:06 2016
+
+@author: abanaei
+"""
+
+import numpy as np
+import pickle
+import formats
+import imp
+
+imp.reload(formats)
+
+
+
+
+
+ano_clean_file_path_2013 = '/DS/data/pmsi/ano13.clean.txt'
+rsa_clean_file_path_2013 = '/DS/data/pmsi/rsa13.clean.txt'
+rehosps_365_list_file_path = '/DS/data/pmsi/rehosps_365_list.txt'
+
+
+# Delais en jours entre deux hospitalisations pour que ca puisse etre considere comme une rehospit
+delai_rehosp = 365
+
+
+codes_ghm_file_path = '../data/codes_ghm.txt'
+codes_cim_file_path = '../data/codes_cim10.txt'
+codes_ccam_file_path = '../data/codes_ccam.txt'
+codes_type_um_file_path = '../data/codes_type_um.txt'
+codes_cmd_file_path = '../data/codes_cmd.txt'
+codes_departement_file_path = '../data/codes_departement.txt'
+codes_type_ghm_file_path = '../data/codes_type_ghm.txt'
+codes_complexity_ghm_file_path = '../data/codes_complexity_ghm.txt'
+
+
+
+codes_ghm_list = list()
+codes_ccam_list = list()
+codes_cim_list = list()
+codes_type_um_list = list()
+codes_complexity_ghm_list = list()
+codes_cmd_list = list()
+codes_departement_list = list()
+codes_type_ghm_list = list()
+column_label_list = list()
+
+
+
+
+#####################################
+#           Functions
+#####################################
+
+
+
+#############  Initialization functions
+
+def fill_codes(codes_file_path, codes_list):
+    """
+    Remplit les listes des referentiels a partir des fichiers texte
+    """
+    with open(codes_file_path) as codes_file:
+        for code in codes_file:
+            codes_list.append(code.strip('\n').strip())
+    codes_list.sort()
+    
+def create_column_labels():
+    column_label_list.append('sex')
+    column_label_list.append('age')
+    column_label_list.append('stay_length')
+    for dpt in codes_departement_list:
+        column_label_list.append('dpt_' + dpt)
+    for type_ghm in codes_type_ghm_list:
+        column_label_list.append('type_ghm_' + type_ghm)
+    for complexity_ghm in codes_complexity_ghm_list:
+        column_label_list.append('complexity_ghm_' + complexity_ghm)
+    for type_um in codes_type_um_list:
+        column_label_list.append('type_um_' + type_um)
+        
+        
+    
+def init():
+    
+    del codes_ghm_list[:]
+    del codes_ccam_list[:]
+    del codes_cim_list[:]
+    del codes_type_um_list[:]
+    del codes_complexity_ghm_list[:]
+    del codes_cmd_list[:]
+    del codes_departement_list[:]
+    del codes_type_ghm_list[:]
+    del column_label_list[:]
+    
+    fill_codes(codes_type_um_file_path, codes_type_um_list)
+    fill_codes(codes_cmd_file_path, codes_cmd_list)
+    fill_codes(codes_departement_file_path, codes_departement_list)
+    fill_codes(codes_type_ghm_file_path, codes_type_ghm_list)
+    fill_codes(codes_complexity_ghm_file_path, codes_complexity_ghm_list)
+    create_column_labels()
+    
+    
+def load_rehosps_list(rehosps_list_file_path):            
+    with open(rehosps_list_file_path) as rehosps_file:
+        return pickle.load(rehosps_file)
+
+
+def get_rsa_data(rsa, rsa_format, verbose=None):
+    rsa = rsa.replace('\n', '')   
+    index = int(rsa[rsa_format['index_sp'] - 1:rsa_format['index_ep']].strip())    
+    sex = int(rsa[rsa_format['sex_sp'] - 1:rsa_format['sex_ep']].strip())    
+    departement = rsa[rsa_format['finess_sp']:rsa_format['finess_sp']+2].strip()
+    cmd = rsa[rsa_format['cmd_sp'] - 1:rsa_format['cmd_ep']].strip()
+    try:
+        age = int(rsa[rsa_format['age_in_year_sp'] - 1:rsa_format['age_in_year_ep']].strip())
+    except ValueError:
+        age = 0
+    stay_length = int(rsa[rsa_format['stay_length_sp'] - 1:rsa_format['stay_length_ep']].strip())
+    type_ghm = rsa[rsa_format['type_ghm_sp']-1:rsa_format['type_ghm_ep']].strip()
+    complexity_ghm = rsa[rsa_format['complexity_ghm_sp']-1:rsa_format['complexity_ghm_ep']].strip()
+    
+    fixed_zone_length = int(rsa_format['fix_zone_length'])
+    nb_aut_pgv = int(rsa[rsa_format['nb_aut_pgv_sp'] - 1:rsa_format['nb_aut_pgv_ep']].strip())
+    aut_pgv_length = int(rsa_format['aut_pgv_length'])
+    nb_suppl_radio = int(rsa[rsa_format['nb_suppl_radio_sp'] - 1:rsa_format['nb_suppl_radio_ep']].strip())
+    suppl_radio_length = int(rsa_format['suppl_radio_length'])
+    nb_rum = int(rsa[rsa_format['nbrum_sp'] - 1:rsa_format['nbrum_ep']].strip())
+    rum_length = int(rsa_format['rum_length'])
+    type_um_offset = int(rsa_format['type_um_offset'])
+    type_um_length = int(rsa_format['type_um_length'])    
+    first_um_sp = fixed_zone_length + (nb_aut_pgv * aut_pgv_length) + (nb_suppl_radio * suppl_radio_length) + type_um_offset
+    type_um_dict = {}
+    for i in range(0, nb_rum):
+        type_um = rsa[first_um_sp: first_um_sp + type_um_length].strip()
+        type_um_dict[type_um] = 1
+        first_um_sp += rum_length
+        
+    return {
+    'index':index,
+    'cmd':cmd,
+    'sex':sex,
+    'dpt':departement,
+    'age':age,
+    'stay_length':stay_length,
+    'type_ghm':type_ghm,
+    'complexity_ghm':complexity_ghm,
+    'type_um':type_um_dict.keys(),
+    'rehosp':0,
+     }
+
+
+def load_rhosps_as_dict(file_path=rehosps_365_list_file_path):
+    '''
+    Cette methode verfifie pour chaque line_number (deuxieme element de la liste des rehosps) si le delai 
+    de rehospitalisation est un point max ou pas (delais 1, 7, 14, 21, ...).
+    Elle retourne un dict sous la forme {line_number:True/False}
+    Le fait de faire partie de cette liste veut dire qu'il s'agit d'un sejour qui a donne lieu a une rehosp.
+    '''
+    rehsops_list = load_rehosps_list(file_path)
+    result = {}
+    delays_7x = np.asarray([1] + range(7,365, 7)) # delays multiple of 7 : 1, 7, 14, 21, ...
+    for rehosp in rehsops_list:
+        line_number = rehosp[1]
+        delay = rehosp[2]
+        is_7x = (delay in delays_7x)
+        result[line_number] = is_7x
+    return result
+
+
+def rsa_to_X(rsa, X, i, cll=column_label_list):
+    X[i, cll.index('sex')]=rsa['sex']
+    X[i, cll.index('age')]=rsa['age']
+    X[i, cll.index('stay_length')]=rsa['stay_length']
+    X[i, cll.index('dpt_' + rsa['dpt'])]=1
+    X[i, cll.index('type_ghm_' + rsa['type_ghm'])]=1
+    X[i, cll.index('complexity_ghm_' + rsa['complexity_ghm'])]=1
+    for t_u in rsa['type_um']:
+        X[i, cll.index('type_um_' + t_u)]=1
+
+
+def get_rsas_rehosps_7x(rehosps_dict, rsas_file_path=rsa_clean_file_path_2013, rsa_format=formats.rsa_2013_format):
+    line_number = 1
+    with open(rsas_file_path) as rsa_file:
+        while True:
+            rsa_line = rsa_file.readline().strip()
+            if (line_number in rehosps_dict):
+                rsa_data = get_rsa_data(rsa_line, rsa_format)
+                
+
+            line_number += 1
+            if (not rsa_line):
+                break
+
+
+
+
+# ########################################################
+#                     WORK AREA
+# ########################################################
+
+rehosps_dict = load_rhosps_as_dict()
+
+
+
