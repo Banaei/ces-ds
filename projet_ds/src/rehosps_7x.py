a355dd4b82041e220298ed11911d6e6df317747b
Alireza <alireza.banaei@atih.sante.fr>
2016-02-09 22:50:10 +0100

    Réorganisation des repertoires et fichiers
    Menage du code

--- projet_ds/src/rehosps_7x.py
+++ projet_ds/src/rehosps_7x.py
@@ -13,36 +13,43 @@ from scipy import sparse
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
 import matplotlib.pyplot as plt
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+import os
 
 imp.reload(formats)
 
 
+data_directory = '/DS/data/pmsi/'
+refs_directory = '../refs/'
+results_directory = '../results/'
 
 
-
-ano_clean_file_path_2013 = '/DS/data/pmsi/ano13.clean.txt'
-rsa_clean_file_path_2013 = '/DS/data/pmsi/rsa13.clean.txt'
-rehosps_365_list_file_path = '/DS/data/pmsi/rehosps_365_list.txt'
-rehosps_180_list_file_path = '/DS/data/pmsi/rehosps_180_list.txt'
-
-
-codes_ghm_file_path = '../data/codes_ghm.txt'
-codes_cim_file_path = '../data/codes_cim10.txt'
-codes_ccam_file_path = '../data/codes_ccam.txt'
-codes_type_um_file_path = '../data/codes_type_um.txt'
-codes_cmd_file_path = '../data/codes_cmd.txt'
-codes_departement_file_path = '../data/codes_departement.txt'
-codes_type_ghm_file_path = '../data/codes_type_ghm.txt'
-codes_complexity_ghm_file_path = '../data/codes_complexity_ghm.txt'
-codes_um_urgences_file_path = '../data/codes_type_um_urg.txt'
-ipe_prives_file_path = '../data/codes_es_prives.txt'
-
-rfe_file_path = '../data/rfe'
+ano_clean_file_path_2013 = data_directory + 'ano13.clean.txt'
+rsa_clean_file_path_2013 = data_directory + 'rsa13.clean.txt'
+rehosps_365_list_file_path = data_directory + 'rehosps_365_list.txt'
+rehosps_180_list_file_path = data_directory + 'rehosps_180_list.txt'
 
 
-X_sparse_file_path = '/DS/data/pmsi/X_sparse_rehosps.npz'
-y_sparse_file_path = '/DS/data/pmsi/y_sparse_rehosps.npz'
-tree_dot_file_name = '../data/tree.dot'
+codes_ghm_file_path = refs_directory + 'codes_ghm.txt'
+codes_cim_file_path = refs_directory + 'codes_cim10.txt'
+codes_ccam_file_path = refs_directory + 'codes_ccam.txt'
+codes_type_um_file_path = refs_directory + 'codes_type_um.txt'
+codes_cmd_file_path = refs_directory + 'codes_cmd.txt'
+codes_departement_file_path = refs_directory + 'codes_departement.txt'
+codes_type_ghm_file_path = refs_directory + 'codes_type_ghm.txt'
+codes_complexity_ghm_file_path = refs_directory + 'codes_complexity_ghm.txt'
+codes_um_urgences_file_path = refs_directory + 'codes_type_um_urg.txt'
+ipe_prives_file_path = refs_directory + 'codes_es_prives.txt'
+
+rfe_file_path = results_directory + 'rfe'
+dtc_file_path = results_directory + 'dtc'
+tree_dot_file_path = results_directory + 'dtc.dot'
+tree_pdf_file_path = results_directory + 'dtc.pdf'
+
+
+X_sparse_file_path = data_directory + 'X_sparse_rehosps.npz'
+y_sparse_file_path = data_directory + 'y_sparse_rehosps.npz'
 
 codes_ghm_list = list()
 codes_ccam_list = list()
@@ -65,7 +72,9 @@ column_label_list = list()
 
 
 
-#############  Initialization functions
+# ############################################################################
+#                                   Initializations
+# ############################################################################
 
 def fill_codes(codes_file_path, codes_list):
     """
@@ -96,8 +105,6 @@ def create_column_labels():
     for type_um in codes_type_um_list:
         column_label_list.append('type_um_' + type_um)
         
-        
-    
 def init():
     
     del codes_ghm_list[:]
@@ -124,12 +131,12 @@ def init():
 
     create_column_labels()
     
+
+# ############################################################################
+#                                      PMSI
+# ############################################################################
+
     
-def load_rehosps_list(rehosps_list_file_path=rehosps_180_list_file_path):            
-    with open(rehosps_list_file_path) as rehosps_file:
-        return pickle.load(rehosps_file)
-
-
 def get_rsa_data(rsa, rsa_format, verbose=None):
     emergency = 0
     private = 0
@@ -180,25 +187,6 @@ def get_rsa_data(rsa, rsa_format, verbose=None):
     'private':private
      }
 
-
-def load_rhosps_as_dict(file_path=rehosps_180_list_file_path):
-    '''
-    Cette methode verfifie pour chaque line_number (deuxieme element de la liste des rehosps) si le delai 
-    de rehospitalisation est un point max ou pas (delais 1, 7, 14, 21, ...).
-    Elle retourne un dict sous la forme {line_number:True/False}
-    Le fait de faire partie de cette liste veut dire qu'il s'agit d'un sejour qui a donne lieu a une rehosp.
-    '''
-    rehsops_list = load_rehosps_list(file_path)
-    result = {}
-    delays_7x = np.asarray([1] + range(7,365, 7)) # delays multiple of 7 : 1, 7, 14, 21, ...
-    for rehosp in rehsops_list:
-        line_number = rehosp[1]
-        delay = rehosp[2]
-        is_7x = (delay in delays_7x)
-        result[line_number] = is_7x
-    return result
-
-
 def rsa_to_X(rsa_data_dict, X, i, cll=column_label_list):
     X[i, cll.index('sex')]=rsa_data_dict['sex']
     X[i, cll.index('age')]=rsa_data_dict['age']
@@ -244,6 +232,9 @@ def get_rsas_rehosps_7x(rehosps_dict, rsas_file_path=rsa_clean_file_path_2013, r
 
     return sparse_X, sparse_y
 
+# ############################################################################
+#                                   Files saving and loading
+# ############################################################################
 
 def save_sparse(filename, array):
     np.savez(filename, data=array.data, indices=array.indices, indptr=array.indptr, shape=array.shape)
@@ -253,27 +244,30 @@ def load_sparse(filename):
     loader = np.load(filename)
     return sparse.csr_matrix((loader['data'], loader['indices'], loader['indptr']), shape=loader['shape'])
 
+def load_rehosps_list(rehosps_list_file_path=rehosps_180_list_file_path):            
+    with open(rehosps_list_file_path) as rehosps_file:
+        return pickle.load(rehosps_file)
 
-def analyse_and_learn(X_data, Y_data, min_depth = 1, max_depth = 10):
-#    print 'Loading data ...'
-#    X_data = load_sparse(X_data_filename)
-#    print 'X_data loaded'
-#    Y_data = load_sparse(Y_data_filename)
-#    print 'Y_data loaded'
-    scores = list()
-    print 'Total population size = ', X_data.shape[0]
-    print 'Total number of features =', X_data.shape[1]
-    print 'Total number of labels =', Y_data.shape[0]
-    print 'Beginning Desicion Tree classification'
-    Y_dense = Y_data.todense()
-    for depth in range(min_depth, max_depth+1):
-        dtc = DecisionTreeClassifier(criterion='gini', max_depth=depth)
-        dtc.fit(X_data, Y_dense)
-        score = dtc.score(X_data, Y_dense)
-        scores.append((depth, score))
-        print 'depth = ', depth, 'score = ', score
-    return dtc
+def load_rhosps_as_dict(file_path=rehosps_180_list_file_path):
+    '''
+    Cette methode verfifie pour chaque line_number (deuxieme element de la liste des rehosps) si le delai 
+    de rehospitalisation est un point max ou pas (delais 1, 7, 14, 21, ...).
+    Elle retourne un dict sous la forme {line_number:True/False}
+    Le fait de faire partie de cette liste veut dire qu'il s'agit d'un sejour qui a donne lieu a une rehosp.
+    '''
+    rehsops_list = load_rehosps_list(file_path)
+    result = {}
+    delays_7x = np.asarray([1] + range(7,365, 7)) # delays multiple of 7 : 1, 7, 14, 21, ...
+    for rehosp in rehsops_list:
+        line_number = rehosp[1]
+        delay = rehosp[2]
+        is_7x = (delay in delays_7x)
+        result[line_number] = is_7x
+    return result
 
+# ############################################################################
+#                                   Graphices
+# ############################################################################
 
 def plot_rehosps_180j(rehosps_list):
     delays = np.zeros((len(rehosps_list),1))
@@ -334,43 +328,87 @@ def plot_rehosps_180j_cumsum(rehosps_list):
     plt.ylabel('Nombre de sejours')
     plt.legend(loc="best")
     plt.show()    
+
+
+
+# ############################################################################
+#                                   Machine learning
+# ############################################################################
+
     
+def feature_select_rfe_logistic_regression(X, y, n, verbose=1):
+    model = LogisticRegression()
+    rfe = RFE(model, n, verbose)
+    rfe = rfe.fit(X_sp, y_sp.todense())
+    return rfe
+
+def learn_tree(X_data, Y_data, min_depth = 1, max_depth = 10):
+    Y_dense = Y_data.todense()
+    scores = list()
+    print 'Total population size = ', X_data.shape[0]
+    print 'Total number of features =', X_data.shape[1]
+    print 'Total number of labels =', Y_data.shape[0]
+    print 'Proportion of 1 in target=', float(np.sum(Y_dense))/Y_dense.shape[0]
+    print 'Beginning Desicion Tree classification'
+    for depth in range(min_depth, max_depth+1):
+        dtc = DecisionTreeClassifier(criterion='gini', max_depth=depth)
+        dtc.fit(X_data, Y_dense)
+        score = dtc.score(X_data, Y_dense)
+        scores.append((depth, score))
+        print 'depth = ', depth, 'score = ', score
+    return dtc
+
+
+
+
+
     
+# ########################################################
+# ########################################################
+# ########################################################
 # ########################################################
 #                     WORK AREA
 # ########################################################
+# ########################################################
+# ########################################################
+# ########################################################
 
+# initializing the parameters
 init()
+
+# plotting rehosps
+rehosps_list = load_rehosps_list()
+plot_rehosps_180j_cumsum(rehosps_list)
+
+# generating features (X) and target(y) sparse matrices and saving them
 rehosps_dict = load_rhosps_as_dict()
 X, y = get_rsas_rehosps_7x(rehosps_dict)
 save_sparse(X_sparse_file_path, X.tocsr())
 save_sparse(y_sparse_file_path, y.tocsr())
-X = load_sparse(X_sparse_file_path)
-y = load_sparse(y_sparse_file_path)
 
-rehosps_list = load_rehosps_list()
-plot_rehosps_180j_cumsum(rehosps_list)
-
+# loading sparse matrices
 X_sp = load_sparse(X_sparse_file_path)
 y_sp = load_sparse(y_sparse_file_path)
 
-float(np.sum(y_sp.todense()))/float(len(y_sp.todense()))
+# Creating a decision tree, and saving it
+dtc = learn_tree(X_sp, y_sp, max_depth = 3)
+with open(dtc_file_path, 'w') as f:
+    pickle.dump(dtc, f)
 
-dtc = analyse_and_learn(X_sparse_file_path, y_sparse_file_path, max_depth = 1)
+# Loading the decision tree, saving it as dot and pdf    
+with open(dtc_file_path) as f:
+    dtc = pickle.load(f)   
+f = tree.export_graphviz(dtc, out_file=tree_dot_file_path, feature_names=column_label_list) # clf: tree classifier
+os.system("dot -Tpdf " + tree_dot_file_path + " -o " + tree_pdf_file_path)
 
-
-from sklearn.feature_selection import RFE
-from sklearn.linear_model import LogisticRegression
-
-model = LogisticRegression()
-rfe = RFE(model, 3, verbose=1)
-rfe = rfe.fit(X_sp, y_sp.todense())
-
+# Recursive feature selection and saving it
+rfe = feature_select_rfe_logistic_regression(X_sp, y_sp)
 with open(rfe_file_path, 'w') as f:
     pickle.dump(rfe, f)
-    
+
+# loading the rfe   
 with open(rfe_file_path) as f:
-    rfe_2 = pickle.load(f)
+    rfe = pickle.load(f)
 
 
 features = rfe.support_
